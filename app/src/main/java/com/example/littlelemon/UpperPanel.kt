c705a2e75225fc9ca0a171bc74ba953dd8ad6763
Yan <ygonz42@gmail.com>
2024-04-06 23:01:37 -0400

    Project Complete

--- app/src/main/java/com/example/littlelemon/UpperPanel.kt
+++ app/src/main/java/com/example/littlelemon/UpperPanel.kt
@@ -5,11 +5,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
